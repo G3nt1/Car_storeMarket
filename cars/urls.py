5f3b8465ad361e8dcb051afff3a04f76e54058e8
G3nt1 <98409625+G3nt1@users.noreply.github.com>
2022-12-11 15:57:55 +0100

    Refactor views

--- cars/urls.py
+++ cars/urls.py
@@ -1,17 +1,17 @@
+from cars.views import cars, authentication
 from django.urls import path
-from . import views
 
 urlpatterns = [
-    path('index', views.Index, name='index'),
-    path('', views.CarShow, name='cars'),
-    path('search/', views.CarSearch, name='cars_search'),
-    path('advanced-search', views.CarAdvancedSearch, name='cars_advanced_search'),
-    path('regcar', views.Register_Car, name='register_car'),
-    path('update-car/<int:pk>', views.Update_Car, name='update_car'),
-    path('car_details/<int:pk>', views.CarDetails, name='details'),
-    path('login/', views.LoginClient, name='login'),
-    path('logout/', views.LogoutClient, name='logout'),
-    path('register/', views.RegClients, name='register'),
+    path('index', cars.Index, name='index'),
+    path('', cars.CarShow, name='cars'),
+    path('search/', cars.CarSearch, name='cars_search'),
+    path('advanced-search', cars.CarAdvancedSearch, name='cars_advanced_search'),
+    path('regcar', cars.Register_Car, name='register_car'),
+    path('update-car/<int:pk>', cars.Update_Car, name='update_car'),
+    path('car_details/<int:pk>', cars.CarDetails, name='details'),
+    path('login/', authentication.LoginClient, name='login'),
+    path('logout/', authentication.LogoutClient, name='logout'),
+    path('register/', authentication.RegClients, name='register'),
     # path('search/', views.Search, name='search'),
 
 ]
--- cars/views/cars.py
+++ cars/views/cars.py
@@ -1,12 +1,11 @@
 from django.contrib import messages
-from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, PageNotAnInteger
 from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
-from .filters import CarsFilter
-from .forms import RegCar, RegUsers, CreateUserForm
-from .models import Cars, CarImage
+from cars.filters import CarsFilter
+from cars.forms import RegCar, RegUsers, CreateUserForm
+from cars.models import Cars, CarImage
 
 
 def get_car_filter(request):
@@ -104,43 +103,3 @@ def CarDetails(request, pk):
     makina = Cars.objects.get(id=pk)
     image = CarImage.objects.filter(model=makina)
     return render(request, 'car_details.html', {'makinat': makina, 'image': image})
-
-
-def LoginClient(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('cars')
-
-    return render(request, 'loginclient.html')
-
-
-def LogoutClient(request):
-    logout(request)
-    return redirect('cars')
-
-
-def RegClients(request):
-    if request.method == 'POST':
-        user_form = CreateUserForm(request.POST)
-        profile_form = RegUsers(request.POST)
-        if profile_form.is_valid() and user_form.is_valid():
-            user = user_form.save()
-            profile = profile_form.save(commit=False)
-            profile.username = user
-            profile.email = user.email
-            profile.phone = request.POST['phone']
-            profile.address = request.POST['address']
-            profile.city = request.POST['city']
-            profile.zip_code = request.POST['zip_code']
-            profile.country = request.POST['country']
-            profile.save()
-            return redirect('login')
-    else:
-        user_form = CreateUserForm()
-        profile_form = RegUsers()
-    context = {'user_form': user_form, 'form': profile_form}
-    return render(request, 'register.html', context)
